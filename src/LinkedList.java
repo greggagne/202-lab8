ed54afbc14fbcc70e9cea0ec83447bd2857c0e8a
greggagne <ggagne@westminsteru.edu>
2023-10-04 13:25:29 -0600

    Initial commit.

---
+++ src/LinkedList.java
@@ -0,0 +1,159 @@
+/**
+ * Implementation of the List interface.
+ * 
+ * This implementation uses a singly-linked list.
+ * 
+ * @author Greg Gagne 
+ *
+ */
+public class LinkedList<T> implements List<T> {
+	// reference to the head of the linked list
+	private Node head;
+
+	// number of elements in the list
+	private int numberOfElements;
+
+	public LinkedList() {
+		head = null;
+	}
+
+	/** 
+	 * Inner class representing a node in the linked list
+	 */
+
+	private class Node
+	{
+		private T data;
+		private Node next;
+
+		private Node(T data) {
+			this(data,null);
+		}
+
+		private Node (T data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+
+	// Methods
+
+	@Override
+	public void add(T item) {
+
+		// adds (appends) an item to the rear of the list
+
+		Node newNode = new Node(item);
+		Node current = head;
+
+		if (isEmpty()) {
+			// special case - first element being added to the list
+			head = newNode;
+		}
+		else {
+			while (current.next != null) {
+				current = current.next;
+			}
+
+			// current now references the last item in the list
+			current.next = newNode;
+		}
+
+		++numberOfElements;
+	}
+
+	@Override
+	public boolean add(T item, int index) {
+		// TO DO
+		
+		return false;
+	}
+
+	@Override
+	public boolean contains(T item) {
+		Node current = head;
+		boolean found = false;
+
+		while (current != null && !found) {
+			if (current.data.equals(item)) {
+				found = true;
+			}
+
+			current = current.next;
+		}
+
+		return found;
+
+	}
+
+	@Override
+	public T get(int index) {
+		// TO DO
+
+		return null;
+	}
+
+	@Override
+	public boolean remove(T item) {
+		// TO DO
+
+		return false;
+	}
+
+	@Override
+	public T remove(int index) {
+		T rv = null;
+
+		if (isEmpty() || index >= numberOfElements) {
+			rv = null;
+		}
+		else if (index == 0) {
+			// special case - first element in the list
+			rv = head.data;
+			head = head.next;
+			numberOfElements--;
+		}
+		else {
+			int currentIndex = 0;
+			Node current = head;
+
+			while (currentIndex < (index - 1)) {
+				current = current.next;
+				currentIndex++;
+			}
+
+			// current references the node we want to remove
+			rv = current.next.data;
+			current.next = current.next.next;
+			numberOfElements--;
+		}
+
+		return rv;
+	}
+
+	@Override
+	public int getLength() {
+		return numberOfElements;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TO DO
+
+		return false;
+	}
+
+	@Override
+	public int getFrequency(T item) {
+		// TO DO
+
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TO DO
+
+	}
+
+}
